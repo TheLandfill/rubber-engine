2736feaa1ab244f327cf40d676863216673d86f2
Joseph Mellor <the.landfill.coding@gmail.com>
2020-07-22 16:50:24 -0500

    First working version of the test

---
+++ bin/gcc-linux/toolchain.cmake
@@ -0,0 +1,24 @@
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_CROSSCOMPILING TRUE)
+set(CMAKE_C_COMPILER gcc)
+set(CMAKE_CXX_COMPILER g++)
+
+add_compile_options(-Wall -Wextra)
+add_compile_options(
+	"$<$<STREQUAL:$<TARGET_PROPERTY:LINKER_LANGUAGE>,CXX>:-std=c++17>"
+	"$<$<STREQUAL:$<TARGET_PROPERTY:LINKER_LANGUAGE>,C>:-std=c99>"
+)
+add_compile_options(
+	"$<$<CONFIG:RELEASE>:-O3>"
+	"$<$<CONFIG:RELEASE>:-ffunction-sections>"
+	"$<$<CONFIG:RELEASE>:-fdata-sections>"
+	"$<$<CONFIG:RELEASE>:-flto>"
+	"$<$<CONFIG:DEBUG>:-O0>"
+	"$<$<CONFIG:DEBUG>:-g>"
+)
+add_link_options(
+	"$<$<CONFIG:RELEASE>:-Wl,--gc-sections>"
+)
+add_link_options(
+	-lX11 -lGL -lpthread -lpng -lstdc++fs
+)
